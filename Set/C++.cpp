e9b2e10cc17f1d79b54c42a3a727922cbb6f4268
Lyu(Kriston) Wang <108577541+Kriston-12@users.noreply.github.com>
2024-01-08 20:02:40 -0500

    Create C++.cpp

---
+++ Set/C++.cpp
@@ -0,0 +1,20 @@
+std::unordered_set<valueType> myset;
+
+// Insertion
+myset.insert(values)
+
+
+// Search or Check value exists
+if (myset.find(value) != myset.end()){}
+
+// Delete
+myset.erase(value)
+
+// Size
+size_t num_elements = mySet.size();
+
+// Iterate
+for (const auto& element : mySet) {
+    // Process element
+}
+
