04019c5839b45eb475b6265a86a0841ae1622edb
Lyu(Kriston) Wang <108577541+Kriston-12@users.noreply.github.com>
2024-01-09 18:07:25 -0500

    Create ArrayList.java

---
+++ List/ArrayList.java
@@ -0,0 +1,24 @@
+// Add
+ArrayList<Integer> list = new ArrayList<>();
+list.add(5); // Adds 5 to the end of the list
+
+// Remove Elements:
+ArrayList<Integer> list = new ArrayList<>();
+list.remove(2); // Removes the element at index 2
+
+// Search
+ArrayList<Integer> list = new ArrayList<>();
+int index = list.indexOf(5); // Returns the index of the element with value 5
+
+// Access
+ArrayList<Integer> list = new ArrayList<>();
+int element = list.get(2); // Gets the element at index 2
+
+// Change
+ArrayList<Integer> list = new ArrayList<>();
+list.set(2, 10); // Sets the element at index 2 to 10
+
+// Equality comparison
+boolean isEqual = ArrayList.equals(array1, array2);
+
+
