bedd54e4d124c1696baf7bfa6b2b4518c69ad048
Lyu(Kriston) Wang <108577541+Kriston-12@users.noreply.github.com>
2024-01-08 20:03:26 -0500

    Create Java.java

---
+++ Set/Java.java
@@ -0,0 +1,27 @@
+import java.util.Set;
+import java.util.HashSet;
+
+public class Main {
+    public static void main(String[] args) {
+        Set<ValueType> mySet = new HashSet<>();
+
+        // Insertion
+        mySet.add(value);
+
+        // Search
+        if (mySet.contains(value)) {
+            // Value found
+        }
+
+        // Deletion
+        mySet.remove(value);
+
+        // Size
+        int num_elements = mySet.size();
+
+        // Iterate
+        for (ValueType element : mySet) {
+            // Process element
+        }
+    }
+}
