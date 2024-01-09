34e75d39529760cb4e4bd430a5e4e5d1fbe9ebe9
Lyu(Kriston) Wang <108577541+Kriston-12@users.noreply.github.com>
2024-01-08 19:16:17 -0500

    Create Python.py

---
+++ Maps/Python.py
@@ -0,0 +1,19 @@
+# Map is called dictionary in python
+dictionary = {}
+
+# Insertion
+dictionary[key] = value
+
+# Access
+value = dictionary[key]
+
+# Check key exists
+if key in dictionary:
+  pass
+
+# Iteration
+for key, value in dictionary.items():
+  print(key, value)
+
+# Remove key-value pairs
+del dictionary[key]
