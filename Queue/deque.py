ccd6356aa28635048542c1a858a2012497853af2
Lyu(Kriston) Wang <108577541+Kriston-12@users.noreply.github.com>
2025-02-08 18:30:31 -0500

    Add .remove() and time complexity for each function

--- Queue/deque.py
+++ Queue/deque.py
@@ -7,23 +7,26 @@ d = deque()
 # Create a deque with initial elements
 d = deque([1, 2, 3])
 
-# Append to the right (end)
+# Append to the right (end) O(1)
 d.append(4)  # deque([1, 2, 3, 4])
 
-# Append to the left (start)
+# Append to the left (start) O(1)
 d.appendleft(0)  # deque([0, 1, 2, 3, 4])
 
-# Remove and return an element from the right (end)
+# Remove and return an element from the right (end) O(1)
 d.pop()  # Returns 4, deque([0, 1, 2, 3])
 
-# Remove and return an element from the left (start)
+# Remove and return an element from the left (start) O(1)
 d.popleft()  # Returns 0, deque([1, 2, 3])
 
-# Modify an element by accessing it directly (indexing is supported)
-d[1] = 10  # deque([1, 10, 3])
+# Remove an element from the deque O(n) 
+d.remove(2) # deque([1, 3])
 
-# Check if an element exists
+# Modify an element by accessing it directly (indexing is supported) O(1)
+d[1] = 10  # deque([1, 10])
+
+# Check if an element exists O(n)
 exists = 10 in d  # True
 
-# Get the index of an element
+# Get the index of an element O(n)
 index = d.index(10)  # 1
